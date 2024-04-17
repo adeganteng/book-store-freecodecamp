719bc309ab9657623fa467a7edeebe565401c8b3
adeganteng <wicaksonoade21@gmail.com>
2024-04-17 19:55:31 +0700

    Belajar MERN STACK di freecodecamp

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+import express, { response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoutes from "./routes/booksRoutes.js";
+import cors from "cors";
+
+
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origin with default of cors (*)
+app.use(cors())
+// Option 2: Allow Custom Origins
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// )
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome To MERN Stack Tutorial");
+});
+
+app.use("/books", booksRoutes)
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`App listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
